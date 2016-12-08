f55f06c1415b7eed9e17cdfee4937399fb4249bf
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2016-12-08 13:11:43 +0100

    version 0.1.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name = "tangods-eigerdectris",
-      version = "0.1.8",
+      version = "0.1.9",
       description = "Device server for the Eiger detector",
       packages =['dectris_eiger'],
       py_modules=['EigerDectris'],
